0c46b2da5172b6766cc45bed9faf90d1fd6b9fd6
Nick Emmerson <nickemmerson619@hotmail.com>
2022-06-04 17:26:29 +0100

    First number success

--- Components/casino-score/casino-digit.js
+++ Components/casino-score/casino-digit.js
@@ -13,13 +13,14 @@ class CasinoDigitElement extends HTMLElement {
   }
 
   constructor() {
-    
+
     super();
     this.power = parseInt(this.getAttribute('power'));
     this.score = parseInt(this.getAttribute('score'));
-    this.digit = getDigit(this.score, this.power);
 
-    this.attachShadow({mode: 'open'});
+    const digit = getDigit(this.score, this.power);
+
+    this.attachShadow({ mode: 'open' });
 
     const styles = document.createElement('link');
     styles.setAttribute('rel', 'stylesheet');
@@ -29,15 +30,14 @@ class CasinoDigitElement extends HTMLElement {
     const content = document.getElementById('casino-digit').content.cloneNode(true);
 
     const changes = [
-      { digit: (this.digit + 1) % 10, className: 'digit-next' },
-      { digit: this.digit, className: 'digit-current' },
-      { digit: (this.digit + 9) % 10, className: 'digit-previous' },
+      { digit: (digit + 1) % 10, className: 'digit-next' },
+      { digit: digit, className: 'digit-current' },
+      { digit: (digit + 9) % 10, className: 'digit-previous' },
     ];
 
     changes.forEach(change => {
       const element = content.getElementById(`digit-${change.digit}`);
-      element.classList.remove('digit-hidden');
-      element.classList.add(change.className);
+      element.classList.replace('digit-hidden', change.className);
     });
 
     this.shadowRoot.appendChild(content);
@@ -49,10 +49,26 @@ class CasinoDigitElement extends HTMLElement {
     switch (attrName) {
       case 'score': {
         const score = parseInt(newVal);
-        // HERE - trying to change classes once new score set.
-        break;
+        if (score !== this.score) {
+          if (score !== this.score + 1) {
+            throw new Error('Functionality currently not avaiable.');
+          }
+          const digitCurrent = getDigit(this.score, this.power);
+          const digitNew = getDigit(score, this.power);
+
+          this.score = score;
+
+          if (digitNew !== digitCurrent) {
+            this.shadowRoot.getElementById(`digit-${(digitCurrent + 9) % 10}`).classList.replace('digit-previous', 'digit-hidden');
+            this.shadowRoot.getElementById(`digit-${digitCurrent}`).classList.replace('digit-current', 'digit-previous');
+            this.shadowRoot.getElementById(`digit-${(digitCurrent + 1) % 10}`).classList.replace('digit-next', 'digit-current');
+            this.shadowRoot.getElementById(`digit-${(digitCurrent + 2) % 10}`).classList.replace('digit-hidden', 'digit-next');
+          }
+        }
       }
-        
+
+        break;
+
       default:
         throw new Error('attrName not found.');
     }
